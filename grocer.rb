9b7cb5bb0e1d74aeac86cd75d7cf56d51c3fc610
Heejae Han <hhan1194@gmail.com>
2019-09-14 15:40:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,30 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.each do |element_hash|
+    element_name = element_hash.keys[0]
+    if final_hash.has_key?(element_name)
+      final_hash[element_name][:count] += 1 
+    else 
+      final_hash[element_name] = {
+        :price => element_hash[element_name][:price],
+        :clearance => element_hash[element_name][:clearance],
+        :count => 1
+      }
+    end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  final_hash = {}  
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart[item] && cart[item][:count] >= coupon[:num]
+      final_hash[]
+    end
+       
 end
 
 def apply_clearance(cart)
