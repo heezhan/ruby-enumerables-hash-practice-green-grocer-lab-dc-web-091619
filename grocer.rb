152d0b44f9edc6167fa65cc43ece13a676a2c356
Heejae Han <hhan1194@gmail.com>
2019-09-14 15:47:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,12 +24,10 @@ def apply_coupons(cart, coupons)
       if cart[name] && cart[name][:count] >= coupon[:num]
         if cart["#{name} W/COUPON"]
           cart["#{name} W/COUPON"][:count] += 1 
-        else 
-          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
-          :clearance => cart[name][:clearance], :count => 1} 
-        end 
-  
-      cart[name][:count] -= coupon[:num] 
+        else
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
+        end
+        cart[name][:count] -= coupon[:num] 
     end 
   end 
 end 
