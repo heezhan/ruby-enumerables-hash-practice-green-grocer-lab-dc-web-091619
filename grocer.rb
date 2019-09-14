3a4e058da1115749617ec6ea235e64a7fd8fd663
Heejae Han <hhan1194@gmail.com>
2019-09-14 16:35:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,13 +37,12 @@ def apply_coupons(cart, coupons)
 end 
 
 def apply_clearance(cart)
-  cart.each do |item, attribute_hash| 
-    if attribute_hash[:clearance] == true 
-      attribute_hash[:price] = (attribute_hash[:price] *
-      0.8).round(2) 
+  cart.each do |item, value|
+    if value[:clearance] == true
+      vale[:price] = (value[:price] * 0.8).round(2)
     end 
-  end 
-cart 
+  end
+  cart 
 end
 
 def checkout(cart, coupons)
